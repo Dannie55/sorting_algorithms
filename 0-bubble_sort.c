c7b3f9d51a0361632b8b3c7fe537a8a2f95ceb5d
Dannie55 <ezirimdaniel080@gmail.com>
2023-04-21 01:18:48 -0700

    fixed bugs

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -5,13 +5,13 @@
  * @a: The first integer to be swapped.
  * @b: The second integer to be swapped.
  */
-void swap_ints(int *a, int*b)
+void swap_ints(int *a, int *b)
 {
-        int tmp;
+	int tmp;
 
-        tmp = *a;
-        *a = *b;
-        *b = tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
 /**
@@ -23,24 +23,24 @@ void swap_ints(int *a, int*b)
  */
 void bubble_sort(int *array, size_t size)
 {
-        size_t i, len = size;
-        bool bubble = false;
+	size_t i, len = size;
+	bool bubble = false;
 
-        if (array == NULL || size < 2)
-                return;
+	if (array == NULL || size < 2)
+		return;
 
-        while (bubble == false)
-        {
-                bubble = true;
-                for (i = 0; i < len - 1; i++)
-                {
-                        if (array[i] > array[i + 1])
-                        {
-                                swap_ints(array + i, array + i + 1);
-                                print_array(array, size);
-                                bubble = false;
-                        }
-                }
-                len --;
-        }
+	while (bubble == false)
+	{
+		bubble = true;
+		for (i = 0; i < len - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap_ints(array + i, array + i + 1);
+				print_array(array, size);
+				bubble = false;
+			}
+		}
+		len--;
+	}
 }
