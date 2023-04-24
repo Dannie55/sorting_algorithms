9d5745dc2da8018a09fc2666648eaadb95d3bc3d
Ay-iman <ayomiderabiu00@gmail.com>
2023-04-24 18:29:44 +0100

    2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -24,22 +24,26 @@ void swap_ints(int *a, int *b)
  */
 void selection_sort(int *array, size_t size)
 {
-	int *max;
-	size_t i, j;
+    size_t i, j, min_idx;
 
-	if (array == NULL || size < 2)
-		return;
+    if (array == NULL || size < 2)
+        return;
 
-	for (i = 0; i < size - 1; i++)
-	{
-		max = array + i;
-		for (j = i + 1; j < size; j++)
-			max = (array[j] < *max) ? (array + j) : max;
-
-		if ((array + i) != max)
-		{
-			swap_ints(array + i, max);
-			print_array(array, size);
-		}
-	}
+    for (i = 0; i < size - 1; i++)
+    {
+        min_idx = i;
+        for (j = i + 1; j < size; j++)
+        {
+            if (array[j] < array[min_idx])
+                min_idx = j;
+        }
+        if (min_idx != i)
+        {
+            int tmp = array[min_idx];
+            array[min_idx] = array[i];
+            array[i] = tmp;
+            print_array(array, size);
+        }
+    }
 }
+
